44b7624178e29353ac09f49bafeaa44ac1b44dfa
Rey <77946015+Reyxalim@users.noreply.github.com>
2021-01-29 11:38:31 +0800

    Update index.js

--- index.js
+++ index.js
@@ -11,12 +11,19 @@ const Token = "YOUR-TOKEN";
 //Create a new client
 const client = new Discord.Client();
 
+//Export your 'client' so we can use it in other files
+module.exports = client
 
+//Read the 'events' directory and using the 'forEach' loop to require each file
 readdirSync('./events/).forEach(file => {
+    //Require the file
     const event = requires(`./events/${file}`);
     
     console.log(`${event.name} event loaded`);
+
+    //Emit the event
     client[event.emitter](event.name, (...params) => event.emit(...params));
 });
 
+//Login your bot
 client.login(Token);
