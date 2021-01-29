70e6dc6282a9c50794ad14025dab443fbf85cd26
Rey <77946015+Reyxalim@users.noreply.github.com>
2021-01-29 11:57:53 +0800

    Update index.js

--- example/index.js
+++ example/index.js
@@ -1,1 +1,30 @@
+// =====  This is an example on how to make an event handler based on the Discord.js lib  =====
 
+
+//Require the 'discord.js' module and the 'fs' module
+const Discord = require('discord.js');
+const { readdirSync } = require('fs');
+
+//Your token
+const Token = "YOUR-TOKEN";
+
+//Create a new client
+const client = new Discord.Client();
+
+//Export your 'client' so we can use it in other files
+module.exports = client
+
+//Read the 'events' directory and using the 'forEach' loop to require each file
+readdirSync('./events/).forEach(file => {
+    //Require the file
+    const event = requires(`./events/${file}`);
+    
+    console.log(`${event.name} event loaded`);
+
+    //Emit the event
+    client[event.emitter](event.name, (...params) => event.emit(...params));
+});
+
+//Login your bot
+client.login(Token);
+© 2021 GitHub, Inc.
